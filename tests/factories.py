a3f7f19cb31202bcc768fa2b594e470780dda068
Pranali-Likhitkar <152873133+Pranali-Likhitkar@users.noreply.github.com>
2024-11-11 11:51:00 +0530

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -22,13 +22,48 @@ from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
 
 
+
+import factory
+from factory.fuzzy import FuzzyChoice, FuzzyDecimal
+from models import Product, Category  # Import Product model and Category if it is an enum or defined elsewhere
+
 class ProductFactory(factory.Factory):
     """Creates fake products for testing"""
-
+    
     class Meta:
         """Maps factory to data model"""
-
         model = Product
-
+    
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    
+    name = FuzzyChoice(
+        choices=[
+            "Hat",
+            "Pants",
+            "Shirt",
+            "Apple",
+            "Banana",
+            "Pots",
+            "Towels",
+            "Ford",
+            "Chevy",
+            "Hammer",
+            "Wrench"
+        ]
+    )
+    
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True, False])
+    
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
+
